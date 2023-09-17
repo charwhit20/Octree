78f1fceafb2290cc8a4efef83c8ddbf79b86abdd
AutoSmelt <charwhit20@gmail.com>
2023-09-17 18:29:16 -0500

    Started creating basis for insert function

--- Octree.py
+++ Octree.py
@@ -1,12 +1,12 @@
 """
-p1 +x +y +z
-p2 +x +y -z
-p3 +x -y +z
-p4 +x -y -z
-p5 -x +y +z
-p6 -x +y -z
-p7 -x -y +z
-p8 -x -y -z
+0 +x +y +z
+1 +x +y -z
+2 +x -y +z
+3 +x -y -z
+4 -x +y +z
+5 -x +y -z
+6 -x -y +z
+7 -x -y -z
 data (x, y, z)
 """
 
@@ -14,22 +14,68 @@ data (x, y, z)
 Each Node denotes a perfect representation of an ideal octant
 contains a list point maximum for each, and points to 8 other octants
 """
+
+
 class Node:
-    def __init__(self, data, quads=(0,0,0,0,0,0,0,0)):
-        self.data = [0,0,0,0,0,0,0,0]
-        self.q1 = quads[0]
-        self.q2 = quads[1]
-        self.q3 = quads[2]
-        self.q4 = quads[3]
-        self.q5 = quads[4]
-        self.q6 = quads[5]
-        self.q7 = quads[6]
-        self.q8 = quads[7]
+    def __init__(self, data=(None, None, None, None, None, None, None, None), quads=(0, 0, 0), depth=0):
+        # tracks depth of each node
+        self.depth = depth
+        # tracks datapoints inside quadrant radius, mutable
+        self.data = list(data)
+        # tracks coords of quadrant (x, y, z), immutable, set at quadrant creation
+        self.quads = quads
+        # tracks nodes each quadrant points to, mutable
+        self.qvals = [None, None, None, None, None, None, None, None]
 
 
 class Octree:
     def __init__(self):
         self.head = Node()
 
-#insert point in octree
+
+"""
+0 +x +y +z
+1 +x +y -z
+2 +x -y +z
+3 +x -y -z
+4 -x +y +z
+5 -x +y -z
+6 -x -y +z
+7 -x -y -z
+data (x, y, z)
+"""
+# insert point in octree
+# octant check, if point in slot in octant != None, create new Node
+# data is (x,y,z)
+
+
 def insert(self, data):
+    cur = self.head
+    x, y, z = data
+    x1, y1, z1 = cur.quads
+    while True:
+        # decipher quadrant point belongs to
+        # quad 0
+        if x > x1 and y > y1 and z > z1:
+            return
+        # quad 1
+        elif x > x1 and y > y1 and z < z1:
+            return
+        # quad 2
+        elif x > x1 and y < y1 and z > z1:
+            return
+        # quad 3
+        elif x > x1 and y < y1 and z < z1:
+            return
+        # quad 4
+        elif x <= x1 and y >= y1 and z >= z1:
+            return
+        # quad 5
+        elif x <= x1 and y >= y1 and z <= z1:
+            return
+        # quad 6
+        elif x <= x1 and y <= y1 and z >= z1:
+            return
+        # quad 7
+        elif x <= x1 and y <= y1 and z <= z1:
+            return
