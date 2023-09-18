b12debfdda00ea3e0a3bb4fa5d63645a41b026c3
AutoSmelt <charwhit20@gmail.com>
2023-09-18 11:16:49 -0500

    Debugged, need to ensure proper downsampling

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 import pandas as pd
 from OctreePackage import Octree
+import pickle
 data = pd.read_csv('input.csv')
 xmax, ymax, zmax = data.abs().max()
 
@@ -8,6 +9,10 @@ print(xmax, ymax, zmax)
 Tree = Octree((xmax, ymax, zmax))
 for index, row in data.iterrows():
     Tree.insert((row['x'], row['y'], row['z']))
+
+
+file = open('processedPoints', 'wb')
+pickle.dump(Tree, file)
 
 clean = Tree.cleanTree()
 print(clean)
