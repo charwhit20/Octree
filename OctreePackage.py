102f2a02c16e46f3b0c9b9bca650bdc21cef16c8
AutoSmelt <charwhit20@gmail.com>
2023-09-18 19:26:18 -0500

    Created new system that randomly removes local clusters of leaves based on threshold

--- OctreePackage.py
+++ OctreePackage.py
@@ -1,4 +1,6 @@
 import pandas as pd
+import random
+import math
 """
 0 +x +y +z
 1 +x +y -z
@@ -16,30 +18,46 @@ Each Node denotes a perfect representation of an ideal octant
 contains a list point maximum for each, and points to 8 other octants
 """
 
+# downsampled pointcloud
+dspc = pd.DataFrame(columns=['x', 'y', 'z'])
+
+
+def save(filename) -> None:
+    global dspc
+    dspc.to_csv(filename, index=False)
+
 
 # This defines an OCTANT, which points to 8 POINTS inside it
 class Octant:
-    maxPoints = 8
+    def is_leaf(self):
+        return all(child is None for child in self.octants)
 
-    def __init__(self, data):
+    def pick_leaf(self):
+        global dspc
+        leaf_data = pd.concat([self.octant0, self.octant1, self.octant2, self.octant3, self.octant4, self.octant5,
+                               self.octant6, self.octant7], ignore_index=True)
+        dspc = pd.concat([dspc, leaf_data], ignore_index=True)
+
+    def __init__(self, data, maxp):
         # UPON CREATION, ASSIGN POINTS TO OCTANTS, UPON DIVISION, ASSIGN DATA TO NEW OCTANTS
+        self.maxPoints = maxp
         # tracks octants each octant points to
         self.octants = [None, None, None, None, None, None, None, None]
         # tracks coords of octant
-        self.xcoord, self.ycoord, self.zcoord = data.mean()
+        self.xcoord = sum(row[0] for row in data) / len(data)
+        self.ycoord = sum(row[1] for row in data) / len(data)
+        self.zcoord = sum(row[2] for row in data) / len(data)
         # after octant has been divided, now append data to each octant
-        self.octant0 = pd.DataFrame()
-        self.octant1 = pd.DataFrame()
-        self.octant2 = pd.DataFrame()
-        self.octant3 = pd.DataFrame()
-        self.octant4 = pd.DataFrame()
-        self.octant5 = pd.DataFrame()
-        self.octant6 = pd.DataFrame()
-        self.octant7 = pd.DataFrame()
-        for index, row in data.iterrows():
-            x = row['x']
-            y = row['y']
-            z = row['z']
+        self.octant0 = []
+        self.octant1 = []
+        self.octant2 = []
+        self.octant3 = []
+        self.octant4 = []
+        self.octant5 = []
+        self.octant6 = []
+        self.octant7 = []
+        for index, row in enumerate(data):
+            x, y, z = row
             """
             0 +x +y +z
             1 +x +y -z
@@ -74,28 +92,66 @@ class Octant:
             # octant 7
             elif x <= self.xcoord and y <= self.ycoord and z <= self.zcoord:
                 self.octant7.append(row)
+
         # if length of octant data is greater than threshold, break it into more octants
         if len(self.octant0) > self.maxPoints:
-            self.octants[0] = Octant(self.octant0)
+            # print("Octant has ", len(self.octant0), " points")
+            self.octants[0] = Octant(self.octant0, self.maxPoints)
+            self.octant0 = None
         if len(self.octant1) > self.maxPoints:
-            self.octants[1] = Octant(self.octant1)
+            # print("Octant has ", len(self.octant1), " points")
+            self.octants[1] = Octant(self.octant1, self.maxPoints)
+            self.octant1 = None
         if len(self.octant2) > self.maxPoints:
-            self.octants[2] = Octant(self.octant2)
+            # print("Octant has ", len(self.octant2), " points")
+            self.octants[2] = Octant(self.octant2, self.maxPoints)
+            self.octant2 = None
         if len(self.octant3) > self.maxPoints:
-            self.octants[3] = Octant(self.octant3)
+            # print("Octant has ", len(self.octant3), " points")
+            self.octants[3] = Octant(self.octant3, self.maxPoints)
+            self.octant3 = None
         if len(self.octant4) > self.maxPoints:
-            self.octants[4] = Octant(self.octant4)
+            # print("Octant has ", len(self.octant4), " points")
+            self.octants[4] = Octant(self.octant4, self.maxPoints)
+            self.octant4 = None
         if len(self.octant5) > self.maxPoints:
-            self.octants[5] = Octant(self.octant5)
+            # print("Octant has ", len(self.octant5), " points")
+            self.octants[5] = Octant(self.octant5, self.maxPoints)
+            self.octant5 = None
         if len(self.octant6) > self.maxPoints:
-            self.octants[6] = Octant(self.octant6)
+            # print("Octant has ", len(self.octant6), " points")
+            self.octants[6] = Octant(self.octant6, self.maxPoints)
+            self.octant6 = None
         if len(self.octant7) > self.maxPoints:
-            self.octants[7] = Octant(self.octant7)
+            # print("Octant has ", len(self.octant7), " points")
+            self.octants[7] = Octant(self.octant7, self.maxPoints)
+            self.octant7 = None
 
 
 class Octree:
     # include max and min so the tree knows how to divide quadrants
-    def __init__(self, data):
-        self.head = Octant(data)
+    def __init__(self, data: pd.DataFrame, maxp=8):
+        self.head = Octant(data.values.tolist(), maxp)
 
-    # Calculate the center of mass of a cloud of data
+    # now with tree created, find all the leaves in the tree
+    def __get_leaf_nodes__(self, octant):
+        leaves = []
+        for i in range(8):
+            child = octant.octants[i]
+            if child is not None:
+                if child.is_leaf():
+                    leaves.append(child)
+                else:
+                    leaves.extend(self.__get_leaf_nodes__(child))
+        return leaves
+
+    # this will randomly select and delete leaves from the tree, then return a new dataframe for the cleaned data
+    def __pick_leaves__(self, threshold):
+        leaves = self.__get_leaf_nodes__(self.head)
+        leaves_to_process = random.sample(leaves, (len(leaves) * math.floor(1 - (1 / threshold))))
+        for leaf in leaves_to_process:
+            leaf.pick_leaf()
+
+    def downsample(self, threshold, filename):
+        self.__pick_leaves__(threshold)
+        save(filename)
--- main.py
+++ main.py
@@ -1,18 +1,6 @@
 import pandas as pd
 from OctreePackage import Octree
-import pickle
+
 data = pd.read_csv('input.csv')
-xmax, ymax, zmax = data.abs().max()
-
-print(xmax, ymax, zmax)
-
-Tree = Octree((xmax, ymax, zmax))
-for index, row in data.iterrows():
-    Tree.insert((row['x'], row['y'], row['z']))
-
-
-file = open('processedPoints', 'wb')
-pickle.dump(Tree, file)
-
-clean = Tree.cleanTree()
-print(clean)
+Tree = Octree(data, 8)
+Tree.downsample(2, 'newdata.csv')
